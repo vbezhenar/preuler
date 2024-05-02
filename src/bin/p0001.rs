baebc5ee066355681403a2480226e9d4f8c3ab42
Vladimir Bezhenar <me@vbezhenar.com>
2024-05-02 15:33:34 +0500

    Rewrite p1 solution

--- src/bin/p0001.rs
+++ src/bin/p0001.rs
@@ -1,9 +1,4 @@
 fn main() {
-  let mut sum = 0;
-  for i in 1 .. 1000 {
-    if i % 3 == 0 || i % 5 == 0 {
-      sum += i;
-    }
-  }
-  println!("{}", sum);
+    let answer = (1..1000).filter(|x| x % 3 == 0 || x % 5 == 0).sum::<u32>();
+    println!("{}", answer);
 }
