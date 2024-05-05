e44e7cf41954f1ca5435384936f9748202f55a09
Vladimir Bezhenar <me@vbezhenar.com>
2024-05-05 21:03:24 +0500

    Move FibonacciIterator to p0002

--- src/bin/p0002.rs
+++ src/bin/p0002.rs
@@ -1,5 +1,3 @@
-use preuler::FibonacciIterator;
-
 fn main() {
     let fi = FibonacciIterator::new();
     let answer = fi
@@ -7,4 +5,27 @@ fn main() {
         .filter(|f| f % 2 == 0)
         .sum::<u64>();
     println!("{}", answer);
+}
+
+struct FibonacciIterator {
+    n0: u64,
+    n1: u64,
+}
+
+impl FibonacciIterator {
+    pub fn new() -> FibonacciIterator {
+        FibonacciIterator { n0: 0, n1: 1 }
+    }
+}
+
+impl Iterator for FibonacciIterator {
+    type Item = u64;
+
+    fn next(&mut self) -> Option<Self::Item> {
+        let f = self.n0;
+        let n2 = self.n0.checked_add(self.n1).unwrap();
+        self.n0 = self.n1;
+        self.n1 = n2;
+        return Some(f);
+    }
 }
--- src/lib.rs
+++
@@ -1,3 +0,0 @@
-mod fibonacci_iterator;
-
-pub use fibonacci_iterator::FibonacciIterator;
