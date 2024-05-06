039074fb2d7b7c5620f89dc7fd77c8bcf51a72fa
Vladimir Bezhenar <me@vbezhenar.com>
2024-05-07 01:20:29 +0500

    Reformat

--- src/bin/p0001.rs
+++ src/bin/p0001.rs
@@ -1,4 +1,4 @@
 fn main() {
     let answer = (1..1000).filter(|x| x % 3 == 0 || x % 5 == 0).sum::<u32>();
-    println!("{}", answer);
+    println!("{answer}");
 }
--- src/bin/p0002.rs
+++ src/bin/p0002.rs
@@ -4,7 +4,7 @@ fn main() {
         .take_while(|f| *f <= 4_000_000)
         .filter(|f| f % 2 == 0)
         .sum::<u64>();
-    println!("{}", answer);
+    println!("{answer}");
 }
 
 struct FibonacciIterator {
